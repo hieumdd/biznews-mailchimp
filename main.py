5fe64ffea9a831f8f6ac88a13f71ac1491cc825d
HM <hieumdd@gmail.com>
2022-09-02 14:13:55 +0700

    feat: Add flask routing

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import json
 
+from flask import Flask, Request, request
+
 from mailchimp import primary_pipeline_service, webhook_service
 
+app = Flask("internal")
 
-def main(request):
+
+@app.route("/")
+def pipeline_controller():
+    return [
+        primary_pipeline_service.get_lists_service(),
+        primary_pipeline_service.get_campaigns_service(),
+    ]
+
+
+@app.route("/webhook")
+def webhook_controller():
+    data = json.loads(request.form["data"])
+    return webhook_service.webhook_service(data["response_body_url"])
+
+
+def main(request: Request):
     print(request)
 
-    if "webhook" in request.path:
-        data = json.loads(request.form["data"])
-        response = webhook_service.webhook_service(data["response_body_url"])
-    else:
-        response = [
-            primary_pipeline_service.get_lists_service(),
-            primary_pipeline_service.get_campaigns_service(),
-        ]
+    ctx = app.test_request_context(path=request.path, data=request.data)
+    ctx.push()
+
+    response = app.full_dispatch_request()
+
+    ctx.pop()
 
     print(response)
 
-    return {"results": response}
+    return {"result": response}
