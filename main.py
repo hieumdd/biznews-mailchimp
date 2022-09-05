8127debd70c846d78e7573c423f8f741ea4cff08
HM <hieumdd@gmail.com>
2022-09-05 22:36:33 +0700

    feat: Add Primary & Export service

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-import json
-
-from flask import Flask, Request, request
-
-from mailchimp import primary_pipeline_service, webhook_service
-
-app = Flask("internal")
+from mailchimp import primary_service, export_service
 
 
-@app.route("/")
-def pipeline_controller():
-    return [
-        primary_pipeline_service.get_lists_service(),
-        primary_pipeline_service.get_campaigns_service(),
-    ]
-
-
-@app.route("/webhook")
-def webhook_controller():
-    data = json.loads(request.form["data"])
-    return webhook_service.webhook_service(data["response_body_url"])
-
-
-def main(request: Request):
+def main(request):
     print(request)
 
-    ctx = app.test_request_context(path=request.path, data=request.data)
-    ctx.push()
+    data: dict = request.get_json()
 
-    response = app.full_dispatch_request()
-
-    ctx.pop()
+    if "export_id" in data:
+        response = export_service.export_service(data["export_id"])
+    else:
+        response = primary_service.primary_service()
 
     print(response)
 
