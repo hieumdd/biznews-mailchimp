5f3af37555cc4cfd6641d650ef9831e10a91fc9d
HM <hieumdd@gmail.com>
2022-09-05 22:46:05 +0700

    ci: Test Export Service

--- mailchimp/export_service.py
+++ mailchimp/export_service.py
@@ -14,9 +14,12 @@ def create_export_task(export_id: str):
 def export_service(export_id: str):
     download_url = repo.get_export_download_url(export_id)
 
+
     if download_url:
+        extract_path = repo.get_archive(download_url)
+        
         response = [
-            service(download_url)
+            service(extract_path)
             for service in [
                 export_pipeline_service.get_campaign_click_details,
                 export_pipeline_service.get_campaign_open_details,
